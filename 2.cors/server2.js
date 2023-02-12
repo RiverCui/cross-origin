9ad24a28ef20f02cd8e613530df042f21e3fa240
River <rivertsuiwork@gmail.com>
2023-02-12 23:11:20 +0800

    cors Access-Origin-Allow-Origin

---
+++ 2.cors/server2.js
@@ -0,0 +1,16 @@
+let express = require('express')
+let app = express()
+let whiteList = ['http://localhost:3000']
+app.use(function (req, res, next) {
+  let origin = req.headers.origin
+  if (whiteList.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  next()
+})
+app.get('/getData', function (req, res) {
+  console.log(req.headers)
+  res.end('World')
+})
+app.use(express.static(__dirname))
+app.listen(4000)
