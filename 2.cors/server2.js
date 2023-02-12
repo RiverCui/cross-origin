e455d29b81337cd112cd6c9fc7297cac2a8b8751
River <rivertsuiwork@gmail.com>
2023-02-12 23:14:17 +0800

    cors Access-Control-Allow-Headers

--- 2.cors/server2.js
+++ 2.cors/server2.js
@@ -5,6 +5,7 @@ app.use(function (req, res, next) {
   let origin = req.headers.origin
   if (whiteList.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Headers', 'name, xxx, xxx')
   }
   next()
 })
