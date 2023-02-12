b2fb977483ee4894b6eee8e9b152e7c30676cbe0
River <rivertsuiwork@gmail.com>
2023-02-13 00:21:10 +0800

    cors Access-Control-Allow-Methods

--- 2.cors/server2.js
+++ 2.cors/server2.js
@@ -6,8 +6,12 @@ app.use(function (req, res, next) {
   if (whiteList.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
     res.setHeader('Access-Control-Allow-Headers', 'name, xxx, xxx')
+    res.setHeader('Access-Control-Allow-Methods', 'PUT')
   }
   next()
+})
+app.put('/getData', function (req, res) {
+  res.end('World')
 })
 app.get('/getData', function (req, res) {
   console.log(req.headers)
